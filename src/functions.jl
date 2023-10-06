c7e178f6ba59d331d06fab8cc5fab3c316e44f26
MatthewHeiden <52428107+MatthewHeiden@users.noreply.github.com>
2023-10-06 18:00:10 -0400

    Minor stylistic edits

--- src/functions.jl
+++ src/functions.jl
@@ -1,136 +1,75 @@
-using Plots
+using Statistics
+Triangle = AbstractArray
 
-function latest_diagonal(◤::AbstractArray)
-    # Copied from https://github.com/JuliaActuary/ChainLadder.jl/blob/master/src/utils.jl
-    return [row[findlast(x -> !ismissing(x), row)] for row in eachrow(◤)]
-end
+latest_diagonal(◤::Triangle) = last.(collect.(skipmissing.(eachrow(◤))))
+
+"""Year-on-Year changes will have one less row than the triangle you put in"""
+YOYs(◤::Triangle) = ◤[2:end, :] ./ ◤[1:end-1, :]
 
 """LDFs will have one less column than the triangle you put in"""
-LDFs(◤::AbstractArray) = ◤[:, 2:end] ./ ◤[:, 1:end-1]
+LDFs(◤::Triangle) = ◤[:, 2:end] ./ ◤[:, 1:end-1]
 
-function latest_three_year_LDFs(◤::AbstractArray)
-
+function latest_three_year_LDFs(◤::Triangle)
     # Create first, second, and third LDFs, where you have <3 data points
     third = sum(◤[1:2, end-1:end-1]) / sum(◤[1:2, end-2:end-2])
     second = ◤[1, end] / ◤[1, end-1]
     first = second
 
+    # Use matrix operations to get most of the LDFs
     three_year_sum = ◤[1:end-2, :] + ◤[2:end-1, :] + ◤[3:end, :]
     three_year_LDFs = LDFs(three_year_sum)
 
-    # Remove last row (all missing values)
+    # Remove last row (all missing)
     three_year_LDFs = three_year_LDFs[1:end-1, :]
-
     latest_three_year_LDFs = latest_diagonal(three_year_LDFs)
-
     return vcat(first, second, third, latest_three_year_LDFs)
-
 end
 
-"""YOYs will have one less row than the triangle you put in"""
-YOYs(◤::AbstractArray) = ◤[2:end, :] ./ ◤[1:end-1, :]
+column_averages(◤::Triangle) = mean.(collect.(skipmissing.(eachcol(◤))))
 
-function column_averages(◤::AbstractArray)
-    col_sum(col) = sum(x for x in col if !ismissing(x))
-    col_count(col) = sum(1 for x in col if !ismissing(x))
-    return [col_sum(col) ./ col_count(col) for col in eachcol(◤)]
-end
-
-function CDFs(◤::AbstractArray, tail_factor::Float64)
-    ldfs = LDFs(◤)
-    average_ldfs = column_averages(ldfs)
+function CDFs(◤::Triangle, tail_factor::Float64=1.0)
+    average_ldfs = column_averages(LDFs(◤))
     push!(average_ldfs, tail_factor)
-
-    # reverse the order to appropriately match the losses later
-    cdfs = cumprod(average_ldfs[end:-1:1])
+    cdfs = cumprod(reverse(average_ldfs))
     return cdfs
 end
 
-CDFs(◤::AbstractArray) = CDFs(◤, 1.0)
-
-function chainladder_ultimates(◤::AbstractArray, tail_factor::Float64)
+function chainladder_ultimates(◤::Triangle, tail_factor::Float64=1.0)
     return latest_diagonal(◤) .* CDFs(◤, tail_factor)
 end
 
-chainladder_ultimates(◤::AbstractArray) = latest_diagonal(◤) .* CDFs(◤)
-
-function three_year_chainladder_ultimates(◤::AbstractArray)
+function three_year_chainladder_ultimates(◤::Triangle)
     ldfs = latest_three_year_LDFs(◤)
-    cdfs = cumprod(ldfs[end:-1:1])
+    cdfs = cumprod(reverse(ldfs))
     return latest_diagonal(◤) .* cdfs
 end
 
-function born_ferg_ultimates(◤::AbstractArray,
-                             earned_premiums::Vector{<:Number},
-                             expected_claims_ratio::Float64,
-                             tail_factor::Float64)
-
+function born_ferg_ultimates(◤::Triangle, earned_premiums::Vector{<:Number},
+                             expected_claims_ratio::Float64, tail_factor::Float64=1.0)
     cdfs  = CDFs(◤, tail_factor)
-    diagonal = latest_diagonal(◤)
-    return diagonal + earned_premiums .* expected_claims_ratio .* (1 .- 1 ./ cdfs)
+    weights = 1 .- 1 ./ cdfs
+    return latest_diagonal(◤) + earned_premiums .* expected_claims_ratio .* weights
 end
 
-function born_ferg_ultimates(◤::AbstractArray,
-                             earned_premiums::Vector{<:Number},
-                             expected_claims_ratio::Float64)
-
-    return latest_diagonal(◤) + earned_premiums .* expected_claims_ratio .* (1 .- 1 ./ CDFs(◤))
-end
-
-function cape_cod_ultimates(◤::AbstractArray, on_level_earned_premiums::Vector{<:Number})
-    cdfs = CDFs(◤)
-    diagonal = latest_diagonal(◤)
-    used_up_premium = on_level_earned_premiums ./ cdfs
-    expected_claims_ratio = sum(diagonal) / sum(used_up_premium)
+function cape_cod_ultimates(◤::Triangle, on_level_earned_premiums::Vector{<:Number})
+    used_up_premium = on_level_earned_premiums ./ CDFs(◤)
+    expected_claims_ratio = sum(latest_diagonal(◤)) / sum(used_up_premium)
     return born_ferg_ultimates(◤, on_level_earned_premiums, expected_claims_ratio)
 end
 
-function make_lower_right_missing(◤::AbstractArray)
-    height, width = size(◤)
-    lower_right_missing_matrix = [x <= y ? 1 : missing for x ∈ 1:height, y ∈ width:-1:1]
+function make_lower_right_missing(◤::Triangle)
+    m, n = size(◤)
+    lower_right_missing_matrix = [x <= y ? 1 : missing for x ∈ 1:m, y ∈ n:-1:1]
     return lower_right_missing_matrix .* ◤
 end
 
-function make_heatmap(◤::AbstractArray)
-
-    # Check this out -- It would be nice to add text
-    # https://discourse.julialang.org/t/annotations-and-line-widths-in-plots-jl-heatmaps/4259
-    # Then maybe I could rename this to display() or something like that
-
-    col_maxs = [maximum(x for x in col if !ismissing(x)) for col in eachcol(◤)]
-    col_mins = [minimum(x for x in col if !ismissing(x)) for col in eachcol(◤)]
-
-    # https://en.wikipedia.org/wiki/Feature_scaling#Rescaling_(min-max_normalization)
-    # It's probably better to use a Normal distribution
-    # But I stuck with min-max normalization for now because that's the default in Excel
-    function rescale(x, max, min)
-        # If there's a column with only one entry, just set it at the midpoint
-        if !ismissing(x) && max == min
-            return 0.5
-        else
-            return (x - min) / (max - min)
-        end
-    end
-
-    normalized_triangle = rescale.(◤, transpose(col_maxs), transpose(col_mins))
-    
-    return Plots.heatmap(normalized_triangle[end:-1:1, :],
-                         c=cgrad([:red, :yellow, :green], [0.5]),
-                         legend=false, xticks=false, yticks=false, border=:none)
-end
-
-function berquist_sherman_disposal(counts::AbstractArray, ultimates::Vector{<:Number})
-
-    # the disposal diagonal needs to be in reverse order
-    disposal_diagonal = (latest_diagonal(counts) ./ ultimates)[end:-1:1]
-    counts = make_lower_right_missing(counts)
-    adjusted_counts = ultimates * disposal_diagonal'
+function berquist_sherman_disposal(counts::Triangle, ultimates::Vector{<:Number})
+    disposal_diagonal = latest_diagonal(counts) ./ ultimates
+    adjusted_counts = ultimates * reverse(disposal_diagonal)'
     return make_lower_right_missing(adjusted_counts)
 end
 
-function berquist_sherman_paid(counts::AbstractArray, paid::AbstractArray, 
-                               ultimates::Vector{<:Number})
-
+function berquist_sherman_paid(counts::Triangle, paid::Triangle, ultimates::Vector{<:Number})
     adjusted_counts = berquist_sherman_disposal(counts, ultimates)
     return paid .* adjusted_counts ./ counts
 end
@@ -138,15 +77,12 @@ end
 """Takes in a ◤ of case reserves, takes the latest diagonal, and applies
 severity trend backwards in time. Returns a ◤ of adjusted case reserves."
 """
-function berquist_sherman_case(case::AbstractArray, severity_trend::Float64)
-
-    height, width = size(case)
-    if height != width
+function berquist_sherman_case(case::Triangle, severity_trend::Float64)
+    m, n = size(case)
+    if m != n
         error("Must use square matrices!")
     end
-
-    diagonal = latest_diagonal(case)
-    severity_trends = [(1 + severity_trend) ^ (height + 1 - (x + y)) for x ∈ 1:height, y ∈ 1:height]
-    adjusted_case = diagonal .* severity_trends
+    severity_trends = [(1 + severity_trend) ^ (m + 1 - (x + y)) for x ∈ 1:m, y ∈ 1:m]
+    adjusted_case = latest_diagonal(case) .* severity_trends
     return make_lower_right_missing(adjusted_case)
 end
